ee4b2e7e41fed18467db78736dc29d0cd678c589
ItchyMichi <rivettm91@gmail.com>
2025-06-03 08:25:35 +0100

    src unpack

---
+++ aligner/alignment_engine.py
@@ -0,0 +1,13 @@
+from typing import List, Tuple
+from parser.subtitle_parser import SubtitleEvent
+
+
+def auto_align(ai_events: List[SubtitleEvent], human_events: List[SubtitleEvent]) -> List[Tuple[int, int]]:
+    length = min(len(ai_events), len(human_events))
+    return [(i, i) for i in range(length)]
+
+
+def refine_alignment_with_anchors(ai_events: List[SubtitleEvent], human_events: List[SubtitleEvent], anchors: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    if anchors:
+        return anchors
+    return auto_align(ai_events, human_events)
